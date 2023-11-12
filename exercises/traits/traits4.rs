edac89e4756b1e787bca5f2d3905508e666806a3
extazystas <2153311+Extazystas@users.noreply.github.com>
2023-11-12 22:14:16 +0200

    Done with basic Traits

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -6,9 +6,6 @@
 //
 // Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
@@ -23,7 +20,7 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types<T: Licensed, U: Licensed>(software: T, software_two: U) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
@@ -43,6 +40,13 @@ mod tests {
     fn compare_license_information_backwards() {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
+
+        assert!(compare_license_types(other_software, some_software));
+    }
+    #[test]
+    fn compare_same_information_backwards() {
+        let some_software = SomeSoftware {};
+        let other_software = SomeSoftware {};
 
         assert!(compare_license_types(other_software, some_software));
     }
